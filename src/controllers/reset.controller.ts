aded5fddd0d65443695be4644b757d2bd0ed2dd9
Vinicius Vellozo <vinicius.vellozo@hotmail.com>
2023-02-28 00:33:47 -0300

    feature: reset controller

--- src/controllers/reset.controller.ts
+++ src/controllers/reset.controller.ts
@@ -1,1 +1,7 @@
-export class ResetController {}
+import { NextFunction, Request, Response } from 'express';
+
+export class ResetController {
+	post(req: Request, res: Response) {
+		return res.send().status(200);
+	}
+}
